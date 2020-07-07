d74a963e41517ca8082970e7d14e1e8bf297ad6f
morettilucash <morettilucash@gmail.com>
2020-07-07 01:30:18 -0300

    auth  + api server

--- backend/src/routes/auth_mw.ts
+++ backend/src/routes/auth_mw.ts
@@ -4,35 +4,35 @@ import { Usuarios } from "../Entities/usuarios";
 
 
 export function jwtAdminMidleware(req: Request, res: Response, next) {
-  // const authString = req.headers['authorization'];
+  const authString = req.headers['authorization'];
 
-  // if (typeof authString === 'string' && authString.indexOf(' ') > -1) {
-  //   const authArray = authString.split(' ');
-  //   const token = authArray[1];
-  //   jwt.verify(token, process.env.PKEY, async (err, decoded: Usuarios) => {
-  //     if (err) {
-  //       res.status(403).send(
-  //         {
-  //           ok: false,
-  //           msg: 'Token no válido: No tiene autorización para este recurso',
-  //           error: err
-  //         });
-  //     } else {
-  //       if (decoded.rol == 'ADMIN') {
-  next();
-  //       } else {
-  //         res.status(403).send({
-  //           ok: false,
-  //           msg: 'Token no válido: No tiene autorización para este recurso',
-  //           error: err
-  //         });
-  //       }
-  //     }
-  //   });
-  // } else {
-  //   res.status(403).send({
-  //     ok: false,
-  //     msg: 'Token no válido: No tiene autorización para este recurso'
-  //   });
-  // }
+  if (typeof authString === 'string' && authString.indexOf(' ') > -1) {
+    const authArray = authString.split(' ');
+    const token = authArray[1];
+    jwt.verify(token, process.env.PKEY, async (err, decoded: Usuarios) => {
+      if (err) {
+        res.status(403).send(
+          {
+            ok: false,
+            msg: 'Token no válido: No tiene autorización para este recurso',
+            error: err
+          });
+      } else {
+        if (decoded.rol == 'ADMIN') {
+          next();
+        } else {
+          res.status(403).send({
+            ok: false,
+            msg: 'Token no válido: No tiene autorización para este recurso',
+            error: err
+          });
+        }
+      }
+    });
+  } else {
+    res.status(403).send({
+      ok: false,
+      msg: 'Token no válido: No tiene autorización para este recurso'
+    });
+  }
 }
